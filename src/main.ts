2ea3f4bccce7fa314df1f9e5f492dbae11f9455a
Rauum <vache.romain30@gmail.com>
2024-06-26 19:17:15 +0200

    Modifications lien api et erreurs cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 8000;
-  await app.listen(PORT);
+  const PORT = process.env.PORT || 8000
+  app.setGlobalPrefix('api', {
+    exclude: [{path: '/', method: RequestMethod.GET }]
+  });
+  app.enableCors({
+    origin: '*',
+    methods: ['*'],
+  })
+  await app.listen(8000);
 }
 bootstrap();
